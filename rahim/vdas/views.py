65b84364b0b2151837add14ab3b0d0e198a722a1
dasthagirivinukonda <47562085+dasthagirivinukonda@users.noreply.github.com>
2019-02-12 08:31:57 -0800

    Update views.py

--- rahim/vdas/views.py
+++ rahim/vdas/views.py
@@ -114,35 +114,6 @@ class PollDetailView(APIView):
 
 
 
-class HomePageView(ListView):
-    model= Employee
-    template_name= 'vdas/home.html'
-
-
-class DetailPageView(DetailView):
-    model=Employee
-    template_name= 'vdas/detail.html'
-    context_object_name= "anything"
-class CreatePageView(CreateView):
-    model=Employee
-    template_name= 'vdas/Create.html'
-
-    fields= '__all__'
-
-
-class UpdatePageView(UpdateView):
-    model=Employee
-    template_name= 'vdas/edit.html'
-
-    fields= ['name','age']
-class DeletePageView(DeleteView):
-    model=Employee
-    template_name= 'vdas/delete.html'
-    success_url= reverse_lazy('home')
-
-
-
-
 from .forms import CustomerForm
 
 def home(request):
